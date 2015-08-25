7b73d87ba65612235edde256818d3696808e5742
James Sherry <james.sherry@novafabrik.com>
2015-08-25 04:45:33 +0100

    notes on pseudoclassical method types

---
+++ pseudoClassicalNotes.js
@@ -0,0 +1,26 @@
+// constructor function
+function MyClass () {
+  var privateVariable; // private member only available within the constructor fn
+
+  this.privilegedMethod = function () { // it can access private members
+    //..
+  };
+}
+
+// A 'static method', it's just like a normal function
+// it has no relation with any 'MyClass' object instance  
+MyClass.staticMethod = function () {};
+
+MyClass.prototype.publicMethod = function () {
+  // the 'this' keyword refers to the object instance
+  // you can access only 'privileged' and 'public' members
+};
+
+var myObj = new MyClass(); // new object instance produced here!
+
+myObj.publicMethod();
+MyClass.staticMethod();
+
+// From http://stackoverflow.com/questions/1635116/javascript-class-method-vs-class-prototype-method
+
+// See also: https://developers.google.com/speed/articles/optimizing-javascript for method efficiency.
